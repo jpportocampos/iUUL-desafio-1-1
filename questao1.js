2581804670e71ada7e67253eb9a02f9bad22b687
PipocaPorto <jpportocampos@gmail.com>
2023-06-27 11:13:57 -0300

    questao 1 finalizada

--- questao1.js
+++ questao1.js
@@ -27,7 +27,8 @@ class Vertice {
 
     //dAB² = (xB – xA)² + (yB – yA)²
     distancia(x2, y2) {
-        return Math.sqrt(((x2 - this.x)^2) + ((y2 - this.y)^2));
+        let d = Math.sqrt(((x2 - this.x) * (x2 - this.x)) + ((y2 - this.y) * (y2 - this.y))); 
+        return d;
     }
 
     move(x2, y2) {
@@ -37,7 +38,7 @@ class Vertice {
     }
 
     equals(v) {
-        if(this.x === v.x && this.y === v.y) {
+        if(this.x == v.x && this.y == v.y) {
             return true;
         }
 
@@ -53,18 +54,18 @@ let y1 = prompt('Digite o Y do primeiro Vértice: ');
 let v1 = new Vertice(x1, y1);
 
 let x2 = prompt('Digite o X do segundo Vértice: ');
-let y2 = prompt('Digite o X do segundo Vértice: '); 
+let y2 = prompt('Digite o Y do segundo Vértice: '); 
 
 let v2 = new Vertice(x2, y2);
 
 let x3 = prompt('Digite o X do terceiro Vértice: ');
-let y3 = prompt('Digite o X do terceiro Vértice: '); 
+let y3 = prompt('Digite o Y do terceiro Vértice: '); 
 
 let v3 = new Vertice(x3, y3);
 
 console.log(v1.distancia(v2.x, v2.y));
 console.log(v2.distancia(v3.x, v3.y));
 console.log(v3.distancia(v1.x, v1.y));
-console.log(v1.move(5, 10));
+console.log(v1.move(4, 4));
 console.log(v1.distancia(v2.x, v2.y));
 console.log(v1.equals(v2));
